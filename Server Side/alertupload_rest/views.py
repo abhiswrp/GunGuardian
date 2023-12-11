7a08724185c7d039cfc66ce0f240164d02e539ad
Aryan Trivedi <aryantrivedi222@gmail.com>
2023-12-12 00:21:25 +0530

    Initial commit

---
+++ Server Side/alertupload_rest/views.py
@@ -0,0 +1,19 @@
+from alertupload_rest.serializers import UploadAlertSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+
+# Upload alert
+@api_view(['POST'])
+
+def post_alert(request):
+    serializer = UploadAlertSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    else:
+        return "Error: Unable to process data!"
+
+    return Response(request.META.get('HTTP_AUTHORIZATION'))
+
